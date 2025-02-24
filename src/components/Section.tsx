c945f613341a8541c474e1ddff772ab8b93274c1
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-02-24 12:26:23 +0700

    refactor: props type section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,9 @@
-export default function Section({ children, title }: { children: React.ReactNode; title: string }) {
+type SectionPropsType = {
+  children: React.ReactNode
+  title: string
+}
+
+export default function Section({ children, title }: SectionPropsType) {
   return (
     <div className="flex flex-col gap-2">
       <h2 className="text-xl font-semibold">{title}</h2>
